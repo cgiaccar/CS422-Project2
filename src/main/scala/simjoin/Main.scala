07aa87e53f24c00e34a8893c7c5e8db105d93e02
dorlafo <dorian.laforest@epfl.ch>
2019-05-18 15:56:16 +0200

    Fix duplicate in simjoin and add result text

--- src/main/scala/simjoin/Main.scala
+++ src/main/scala/simjoin/Main.scala
@@ -14,14 +14,14 @@ import java.io._
 object Main {
 
   def main(args: Array[String]) {     
-    //val inputFile="/user/cs422-group20/testEx2/dblp_small.csv"
-    val inputFile="../dblp_10.csv"
-    val numAnchors = 4
+    val inputFile="/user/cs422-group20/testEx2/dblp_10K.csv"
+    //val inputFile="../dblp_small.csv"
+    //val numAnchors = 12
     val distanceThreshold = 2
     val attrIndex = 0
 
-    val input = new File(getClass.getResource(inputFile).getFile).getPath
-    val sparkConf = new SparkConf().setAppName("CS422-Project2").setMaster("local[*]")
+    //val input = new File(getClass.getResource(inputFile).getFile).getPath
+    val sparkConf = new SparkConf().setAppName("CS422-Project2")//.setMaster("local[*]")
     val ctx = new SparkContext(sparkConf)
     val sqlContext = new org.apache.spark.sql.SQLContext(ctx)
     
@@ -30,35 +30,33 @@ object Main {
     .option("header", "false")//.option("header", "true") header->false because the given test files don't have a header
     .option("inferSchema", "true")
     .option("delimiter", ",")
-    .load(input)//.load(inputFile)
+    /*.load(input)*/.load(inputFile)
     
     val rdd = df.rdd        
     val schema = df.schema.toList.map(x => x.name)    
-    val dataset = new Dataset(rdd, schema)           
-    
-
-    val t1 = System.nanoTime    
-    val sj = new SimilarityJoin(numAnchors, distanceThreshold)
-    val res = sj.similarity_join(dataset, attrIndex)
-
-    val resultSize = res.count
-    println("SimJoin count: " + resultSize)
-    val t2 = System.nanoTime
-            
-    println("SimJoin time: " + (t2-t1)/(Math.pow(10,9)))
-    println("SimJoin result: ")
-    res.collect().foreach(println)
-    
+    val dataset = new Dataset(rdd, schema)
 
     // cartesian
     val t1Cartesian = System.nanoTime
     val cartesian = rdd.map(x => (x(attrIndex), x)).cartesian(rdd.map(x => (x(attrIndex), x)))
-                                   .filter(x => (x._1._2(attrIndex).toString() != x._2._2(attrIndex).toString() && Distance.distance(x._1._2(attrIndex).toString(), x._2._2(attrIndex).toString()) <= distanceThreshold))
-                                   
-    println("Cartesian count: " + cartesian.count)
+      .filter(x => (x._1._2(attrIndex).toString() != x._2._2(attrIndex).toString() && Distance.distance(x._1._2(attrIndex).toString(), x._2._2(attrIndex).toString()) <= distanceThreshold))
+    val cartesianSize = cartesian.count
+    println("Cartesian count: " + cartesianSize)
     val t2Cartesian = System.nanoTime
     println("Cartesian time: " + (t2Cartesian-t1Cartesian)/(Math.pow(10,9)))
-    println("Cartesian result: ")
-    cartesian.collect().foreach(println)
+    
+    val numAnchorsList: List[Int] = List(4, 8, 16, 32, 64)//List(2, 4, 6, 8, 10, 12, 14)
+    for (numAnchors <- numAnchorsList) {
+      val t1 = System.nanoTime
+      val sj = new SimilarityJoin(numAnchors, distanceThreshold)
+      val res = sj.similarity_join(dataset, attrIndex)
+
+      val resultSize = res.count
+      println("SimJoin count: " + resultSize)
+      val t2 = System.nanoTime
+
+      println("SimJoin time: " + (t2-t1)/(Math.pow(10,9)))
+      assert(resultSize == cartesianSize)
+    }
   }     
 }
--- src/main/scala/simjoin/SimilarityJoin.scala
+++ src/main/scala/simjoin/SimilarityJoin.scala
@@ -17,17 +17,17 @@ class SimilarityJoin(numAnchors: Int, distThreshold:Int) extends java.io.Seriali
    * that attribute.
    * */  
   def similarity_join(dataset: Dataset, attrIndex: Int) : RDD[(String, String)] = {
-    rdd = dataset.getRDD().map(r => r.getString(attrIndex))
+    rdd = dataset.getRDD().map(r => r.getString(attrIndex)).persist()
 
     val anchors : Array[String] = rdd.takeSample(false, numAnchors)
     //For debug
     //******************************************************************************************************************
-    println("Anchors:")
-    anchors.foreach(println)
+    //println("Anchors:")
+    //anchors.foreach(println)
     //******************************************************************************************************************
     val partitions: Array[RDD[String]] = partitioning(anchors)
 
-    (for(par <- partitions) yield computeJoin(par)).reduce(_ union _)
+    ((for(par <- partitions) yield computeJoin(par)).reduce(_ union _)).distinct()
   }
 
 
@@ -36,30 +36,30 @@ class SimilarityJoin(numAnchors: Int, distThreshold:Int) extends java.io.Seriali
 
     val distancesToAnchors : RDD[(String, List[Int])] = column.map(//list of distances from a point to each anchors
       element => (element, computeDistances(element, anchors))
-    )
+    ).persist()
 
     // For debug
     //******************************************************************************************************************
-    val d1 = distancesToAnchors.collect()
-    println("Distances to anchors:")
-    d1.foreach(println)
+    //val d1 = distancesToAnchors.collect()
+    //println("Distances to anchors:")
+    //d1.foreach(println)
     //******************************************************************************************************************
 
-    val res = for ((_,i) <- anchors.zipWithIndex) yield distancesToAnchors.filter(el => keepInPartition(el._2, i)).map(_._1)
+    val res = for ((_,i) <- anchors.zipWithIndex) yield distancesToAnchors.filter(el => keepInPartition(el._2, i)).map(_._1).persist()
 
     // For debug
     //******************************************************************************************************************
-    val innerPartition = for ((_,i) <- anchors.zipWithIndex) yield distancesToAnchors.filter(el => keepInInnerPartition(el._2, i)).map(_._1)
+    //val innerPartition = for ((_,i) <- anchors.zipWithIndex) yield distancesToAnchors.filter(el => keepInInnerPartition(el._2, i)).map(_._1)
 
-    val outerPartition = for ((_,i) <- anchors.zipWithIndex) yield distancesToAnchors.filter(el => keepInOuterPartition(el._2, i)).map(_._1)
+    //val outerPartition = for ((_,i) <- anchors.zipWithIndex) yield distancesToAnchors.filter(el => keepInOuterPartition(el._2, i)).map(_._1)
 
-    for ((anchors,i) <- anchors.zipWithIndex) {
-      println("Anchors: " + anchors)
-      println("InnerPartition:")
-      innerPartition(i).collect().foreach(println)
-      println("OuterPartition:")
-      outerPartition(i).collect().foreach(println)
-    }
+    //for ((anchors,i) <- anchors.zipWithIndex) {
+    //  println("Anchors: " + anchors)
+    //  println("InnerPartition:")
+    //  innerPartition(i).collect().foreach(println)
+    //  println("OuterPartition:")
+    //  outerPartition(i).collect().foreach(println)
+    //}
     //******************************************************************************************************************
 
     res
@@ -105,17 +105,21 @@ class SimilarityJoin(numAnchors: Int, distThreshold:Int) extends java.io.Seriali
   }
 
   def computeJoin (partition: RDD[String]): RDD[(String, String)] = {
-    println("Partition :")
-    partition.collect().foreach(println)
+    // For debug
+    //******************************************************************************************************************
+    //println("Partition :")
+    //partition.collect().foreach(println)
+    //******************************************************************************************************************
+    println("partition size : " + partition.count())
 
-    val cartesianProduct: RDD[(String, String)] = partition.cartesian(partition)
+    val cartesianProduct: RDD[(String, String)] = partition.cartesian(partition).persist()
 
     val res = cartesianProduct.filter{case (s1:String, s2:String) => if (s1 != s2 && Distance.distance(s1, s2) <= distThreshold) true else false}
     // For debug
     //******************************************************************************************************************
-    val resP = res.collect()
-    println("Join result: ")
-    resP.foreach(println)
+    //val resP = res.collect()
+    //println("Join result: ")
+    //resP.foreach(println)
     //******************************************************************************************************************
     res
   }
